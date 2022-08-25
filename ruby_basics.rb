35599ac4b393ecb6933a4d3f936a8010e36a50e9
maikalangi <maikalangi@gmail.com>
2022-08-24 17:17:41 -0700

    initial commit for ruby tut

---
+++ ruby_basics.rb
@@ -0,0 +1,21 @@
+#Ruby Basics
+# single line comment
+
+=begin
+This is a
+multiline comment
+but most "Ruby-ists"
+don't approve of
+this way and instead 
+opt for the hashtags
+=end
+
+# String
+
+puts "This is a string with double quotes"
+puts 'here is one with single quotes'
+
+# String Methods
+
+puts "hello world".length
+puts "hello world".methods.size
