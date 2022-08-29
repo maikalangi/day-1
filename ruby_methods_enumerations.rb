0046d11720e1e1fe42d157b3e3762e9dd1372f6b
maikalangi <maikalangi@gmail.com>
2022-08-28 22:09:10 -0700

    ruby methods and enumerations

---
+++ ruby_methods_enumerations.rb
@@ -0,0 +1,16 @@
+# rstrip
+
+p " hello ".rstrip
+
+# rstrip! returns 'nil' if nothing changed
+
+p " hello".rstrip! # no space before method call
+
+# reverse
+
+p "stressed".reverse
+
+# reverse!
+# reverses string in place unlike regular reverse where the returned string is a new string
+
+p "stressed".reverse!
